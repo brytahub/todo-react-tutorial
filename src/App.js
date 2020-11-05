c0978b87db5430df3e6e962311c254fc530e9aba
Abiodun Sulaiman <hello@abiodun.dev>
2020-11-05 16:51:49 +0100

    Updated Todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+import "./app.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const [newTodo, setNewTodo] = useState("")
+    const [todos, setTodos] = useState([
+        {id: 1, name: "John", completed: true},
+        {id: 2, name: "Paul", completed: false},
+        {id: 3, name: "Peter", completed: true},
+    ])
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const todoToAdd = {id: new Date().getTime(), name: newTodo, completed: false}
+        setTodos([...todos, todoToAdd])
+        setNewTodo("")
+    }
+
+    function handleDeleteTodo(tdId) {
+        const filteredTodos = todos.filter(todo => todo.id !== tdId)
+        // const filteredTodos = todos.filter(function(todo) {
+        //     return todo.id !== tdId
+        // })
+        setTodos(filteredTodos)
+    }
+    function toggleComplete(tdId) {
+        const toggledTodos = todos.map(todo => todo.id === tdId ? {...todo, completed: !todo.completed} : todo)
+        setTodos(toggledTodos)
+    }
+
+    return (
+        <div className="container">
+            <div className="todo-container">
+                <div className="todo-title">Todo List ({todos.length})</div>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <input className="add-todo" type="text" value={newTodo}
+                        onChange= {(e) => setNewTodo(e.target.value)}
+                     />
+                </form>
+                <div className="todos">
+                    {todos.map((todo) => (
+                        <div className="todo" key={todo.id}>
+                            <button 
+                                className = {`todo-name ${todo.completed ? "todo-completed" : "todo-incomplete"}`}
+                                onClick = {() => toggleComplete(todo.id)}
+                                
+                            >
+                            {todo.name}
+                            </button>
+                            <button onClick = {() => handleDeleteTodo(todo.id)}>Delete Todo</button>
+                        </div>
+                    ))}
+            </div>
+            </div>
+            
+        </div>
+    )
 }
 
 export default App;
